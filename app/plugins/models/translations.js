99fd161a016d5ada9873dde0a9f208062bf02fe7
Saeed <saeed.uni68@gmail.com>
2020-09-20 11:40:49 +0430

    Translations db-plugin has been added

---
+++ app/plugins/models/translations.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const sanitize = require('../utils/sanitizer');
+const mongoose = require('mongoose');
+
+const Model = mongoose.model('Translations');
+
+
+
+const create = async function (data) {
+  let error   = null;
+  let created = null;
+
+  try {
+    created = await Model.create(sanitize(data));
+  } catch (ex) {
+    error = ex;
+  }
+
+  return {
+    created,
+    error
+  };
+};
+
+const findOne = async function (query) {
+  let error  = null;
+  let record = null;
+
+  try {
+    record = await Model.findOne({});
+  } catch (ex) {
+    error = ex;
+  }
+
+  return {
+    record,
+    error
+  };
+};
+
+
+const findByQuery = async function (query) {
+  let error = null;
+  let Record  = null;
+
+  try {
+    Record = await Model.findOne({ query: sanitize(query) });
+  } catch (ex) {
+    error = ex;
+  }
+
+  return {
+    Record,
+    error
+  };
+};
+
+const deleteRecord = async function (_id) {
+  let error = null;
+
+  try {
+    await Model.deleteOne({ _id });
+  } catch (ex) {
+    error = ex;
+  }
+
+  return {
+    error
+  };
+};
+
+
+module.exports = {
+  create,
+  findOne,
+  findByQuery,
+  deleteRecord
+}
