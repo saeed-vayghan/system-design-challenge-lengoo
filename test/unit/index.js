89f84055a1939fb8b5d41624198e98e7d9575613
Saeed <saeed.uni68@gmail.com>
2020-09-20 20:57:36 +0430

    Add some comments to unit tests

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,5 +1,17 @@
 'use strict';
 
+/*
+  Each test should consist below parts:
+    - What is going to be tested?
+    - What circumstances and scenario?
+    - What is the expected result?
+
+  Consider AAA pattern:
+    Arrange: All the setup code to bring the system to the scenario the test aims to simulate.
+    Act: Execute the unit under test.
+    Assert: Ensure that the received value satisfies the expectation.
+*/
+
 require('dotenv').config();
 require('../../config');
 
