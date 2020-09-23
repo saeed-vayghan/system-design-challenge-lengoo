83d7ee7846f4be334bb227186e9de653d208a6db
root <root@localhost.localdomain>
2020-09-23 12:56:18 +0330

    Initiate admin feed API

--- app/controllers/admin/translations/index.js
+++ app/controllers/admin/translations/index.js
@@ -1,10 +1,12 @@
 'use strict';
 
-// const TranslationPlugin = require('../../../plugins/models/translations')
-// const { APIError } = require('../../../plugins/middlewares/error');
+const TranslationPlugin = require('../../../plugins/models/translations')
+const { APIError } = require('../../../plugins/middlewares/error');
 
 
 const feed = async function (req, res, next) {
+
+  return res.json(req.body)
 
 };
 
