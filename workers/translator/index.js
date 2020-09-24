668958de14bbe25104ba28453c867545daec49ba
root <root@localhost.localdomain>
2020-09-24 15:24:36 +0330

    Using database models in workers

--- workers/translator/index.js
+++ workers/translator/index.js
@@ -1,11 +1,16 @@
 'use strict';
 
+const SubtitlePlugin = require('../../app/plugins/models/subtitles');
+const producer       = require('../../app/plugins/thirdParty/rabbit/producer')
+
 
 const translator = async (message) => {
   const status = true;
   const error  = null;
 
-  console.log('===> translator ===> message ==>', message);
+  const result = await SubtitlePlugin.findOne();
+
+  console.log('===> translator ===> message ==>', message, '===> result', result);
 
   return {
     status,
